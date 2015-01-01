eb83816623db9e7372c66cf7cad2d8adb54ae0ec
oanasuciu <osuciu@insa-rennes.fr>
2015-01-01 20:17:00 +0100

    hexagonal grid

--- UI/MapView.cs
+++ UI/MapView.cs
@@ -24,9 +24,9 @@ namespace UI
             if (DesignerProperties.GetIsInDesignMode(this))
                 return;
 
-            Rect myRect = new Rect(0, 0, 700, 700);
+            /*Rect myRect = new Rect(0, 0, 700, 700);
             BitmapImage imag = new BitmapImage(new Uri("textures\\homepage.jpg", UriKind.Relative));
-            dc.DrawImage(imag, myRect);
+            dc.DrawImage(imag, myRect);*/
 
             var difficulty = new SmallMapStrategy();
             INewGameBuilder newGameBuilder = new NewGameBuilder();
@@ -37,26 +37,56 @@ namespace UI
             var game = gameCreator.CreateGame().GetGame();
             var map = game.Map;
 
-            for (var i = 0; i < difficulty.GetMapWidth(); ++i)
+            //Liste des tiles possibles
+            var tilesBitmap = new List<BitmapImage>();
+            tilesBitmap.Add(new BitmapImage(new Uri("textures\\plaine.png", UriKind.Relative)));
+            tilesBitmap.Add(new BitmapImage(new Uri("textures\\foret.png", UriKind.Relative)));
+            tilesBitmap.Add(new BitmapImage(new Uri("textures\\desert.png", UriKind.Relative)));
+            tilesBitmap.Add(new BitmapImage(new Uri("textures\\ocean.png", UriKind.Relative)));
+
+            //Taille des tiles
+            var tileWidth = tilesBitmap[0].Width;
+            var tileHeight = tilesBitmap[0].Height;
+
+            //Décalage pour les lignes impaires
+            var decalageVertical = (tileHeight - ((Math.Sqrt(Math.Pow(tileHeight / 2, 2) - Math.Pow(tileWidth / 2, 2))) * 2)) / 2;
+            var decalageHorizontal = tileWidth / 2;
+
+            double posX, posY;
+            for (var i = 0; i < difficulty.GetMapWidth(); ++i) // i -> y
             {
-                for (var j = 0; j < difficulty.GetMapWidth(); ++j)
+                for (var j = 0; j < difficulty.GetMapWidth(); ++j) // j -> x
                 {
+                    if (j % 2 == 0)
+                    {
+                        posX = i * tileWidth;
+                        posY = j * tileHeight - (decalageVertical * (j - 1));
+                    }
+                    else
+                    {
+                        posX = i * tileWidth + decalageHorizontal;
+                        posY = j * tileHeight - (decalageVertical * (j - 1));
+                    }
+                    posY -= decalageVertical;
+
                     var tile = map.TileAtPosition(new Position(i, j));
+
                     if (tile.IsDesert())
                     {
-
+                        dc.DrawImage(tilesBitmap[2], new Rect(posX, posY, tileWidth, tileHeight));
                     }
                     else if (tile.IsForest())
                     {
-
+                        dc.DrawImage(tilesBitmap[1], new Rect(posX, posY, tileWidth, tileHeight));
                     }
                     else if (tile.IsMountain())
                     {
-
+                        //Ocean ?
+                        dc.DrawImage(tilesBitmap[3], new Rect(posX, posY, tileWidth, tileHeight));
                     }
                     else if (tile.IsPlain())
                     {
-
+                        dc.DrawImage(tilesBitmap[0], new Rect(posX, posY, tileWidth, tileHeight));
                     }
                 }
             }
